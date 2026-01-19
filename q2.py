b325f5a8163af6a62a200107e03f238b5d80f280
sjbuilds <161013465+sjbuilds@users.noreply.github.com>
2026-01-19 17:33:48 +0530

    Update q2.py

--- q2.py
+++ q2.py
@@ -65,7 +65,14 @@ def compressed_stack_length(lst):
         0
     """
     # TODO: Implement your solution here
-    pass
+    n = lst.count()
+    tst()
+    while(i<n):
+      if(lst(i) not in tst()):
+        tst.append(lst(i)
+      if(lst(i) in tst()):
+        tst.pop(lst(i)
+    return tst()
 
 
 if __name__ == "__main__":
